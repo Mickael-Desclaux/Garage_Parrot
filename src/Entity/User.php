e2a54e327a25dd9b343c24e40f23f92abf47ab3e
Mickael <mickaeldesclaux@gmail.com>
2023-06-23 19:03:45 +0200

    Define ROLE_USER to all new users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -37,6 +37,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     
     public function __construct(UserPasswordHasherInterface $passwordHasher) {
         $this->passwordHasher = $passwordHasher;
+        $this->roles = ['ROLE_USER'];
         //$this->cars = new ArrayCollection();
     }
 
