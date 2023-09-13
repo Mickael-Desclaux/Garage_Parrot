6db177611fd2cc282dbea882cab48651d4c8e3d2
Mickael <mickaeldesclaux@gmail.com>
2023-09-13 14:54:11 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+Request::setTrustedProxies(
+// Trust all proxies
+    ['0.0.0.0/0'],
+    // Only trust X-Forwarded-Proto header
+    Request::HEADER_X_FORWARDED_PROTO
+);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
